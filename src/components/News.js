d7d71a56439ef8d1f596ef7e00c91b57a04389cf
subhankar <subhankarmondal0098@gmail.com>
2023-12-05 10:32:37 +0530

    last update is spinner and navbar items update
    last update is spinner and navbar items update

---
+++ src/components/News.js
@@ -0,0 +1,200 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export default class News extends Component {
+  // articles = [
+  //   {
+  //     source: { id: "bbc-sport", name: "BBC Sport" },
+  //     author: null,
+  //     title: "India v Australia - Cricket - BBC Sport",
+  //     description:
+  //       "Find out the in depth batting and bowling figures for India v Australia in the Men's International Twenty20 Match on BBC Sport.",
+  //     url: "http://www.bbc.co.uk/sport/cricket/scorecard/ECKE1216237",
+  //     urlToImage:
+  //       "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.23.3/images/bbc-sport-logo.png",
+  //     publishedAt: "2023-11-26T13:37:19.5816718Z",
+  //     content:
+  //       "<table><tr><th>Batter</th><th>How Out</th><th>Bowler</th><th>Runs</th><th>Balls</th><th>4s</th><th>6s</th><th>Mins</th><th>SR</th></tr>\r\n<tr><th>Total</th><th>(0.1 overs)</th><th>0-for0wickets</th><t… [+1027 chars]",
+  //   },
+  //   {
+  //     source: { id: "al-jazeera-english", name: "Al Jazeera English" },
+  //     author: "Kevin Hand",
+  //     title: "LIVE: India vs Australia: Second T20 cricket match",
+  //     description:
+  //       "Follow our live updates from the second India vs Australia T20 cricket match at Thiruvananthapuram.",
+  //     url: "http://www.aljazeera.com/sports/liveblog/2023/11/26/live-india-vs-australia-second-t20-of-five-match-cricket-series",
+  //     urlToImage:
+  //       "https://www.aljazeera.com/wp-content/uploads/2023/11/AP23327630177989-1700760870.jpg?resize=1920%2C1440",
+  //     publishedAt: "2023-11-26T11:30:27Z",
+  //     content:
+  //       "blinking-dot\r\nLive updatesLive updates, \r\nFollow our live updates from the second India vs Australia T20 cricket match at Thiruvananthapuram, starting at 13:30 GMT.",
+  //   },
+  //   {
+  //     source: { id: "espn-cric-info", name: "ESPN Cric Info" },
+  //     author: null,
+  //     title:
+  //       "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+  //     description:
+  //       "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+  //     url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+  //     urlToImage:
+  //       "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+  //     publishedAt: "2020-04-27T11:41:47Z",
+  //     content:
+  //       "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+  //   },
+  //   {
+  //     source: { id: "espn-cric-info", name: "ESPN Cric Info" },
+  //     author: null,
+  //     title:
+  //       "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+  //     description:
+  //       "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+  //     url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+  //     urlToImage:
+  //       "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+  //     publishedAt: "2020-03-30T15:26:05Z",
+  //     content:
+  //       "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+  //   },
+  // ]; 
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+  
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+ 
+  constructor() {
+    super();
+    // console.log("this is a constructor from news components.");
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ce294ddc637649e78e8be2641430065d&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  handlePrevClick = async () => {
+    // console.log("Previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=ce294ddc637649e78e8be2641430065d&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+  handleNextClick = async () => {
+    // console.log("Next");
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalArticles / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${this.props.category}&apiKey=ce294ddc637649e78e8be2641430065d&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true,
+      });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading: false,
+      });
+    }
+  };
+  render() {
+    return (
+      <>
+        <div className="container my-3">
+          <h2 className="text-center" style={{margin: "35px 0px"}}>NewsMonkey - Top Headlines</h2>
+          {this.state.loading && <Spinner />}
+          <div className="row">
+            {!this.state.loading &&
+              this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imageUrl={
+                        !element.urlToImage
+                          ? "https://images.indianexpress.com/2023/11/Manchester-United-Alejandro-Garnacho-ap-CROP.png?w=640"
+                          : element.urlToImage
+                      }
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark mb-3"
+            onClick={this.handlePrevClick}
+          >
+            &larr;Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalArticles / this.props.pageSize)
+            }
+            className="btn btn-dark mb-3"
+            onClick={this.handleNextClick}
+          >
+            Next&rarr;
+          </button>
+        </div>
+      </>
+    );
+  }
+}
